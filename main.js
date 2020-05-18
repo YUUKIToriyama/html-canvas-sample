45f96cbbe435fe9597a5ef2ffb0a820ee3c1b9e4
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-05-18 20:49:29 +0900

    add some fixes

--- main.js
+++ main.js
@@ -1,41 +1,13 @@
 /* main.js */
 
-var zoom;
+var zoom = 1;
 // 読み込みボタンを押すと画像が読み込まれる
 const loadLocalImage = (event) => {
 	var target = event.target;
-
 	if (!target.files.length) {
 		alert("ファイルが選択されていません");
 		return;
 	}
-
-	/*
-	var file = target.files[0];
-
-	const canvas = document.getElementById("picture");
-	if (canvas.getContext) {
-		const context = canvas.getContext("2d");
-		const image = new Image();
-		const filereader = new FileReader();
-
-		filereader.onload = (ev) => {
-			image.onload = () => {
-				zoom = 500 / image.naturalWidth;
-				var width = 500;
-				var height = zoom * image.naturalHeight;
-				canvas.setAttribute("width", width + "px");
-				canvas.setAttribute("height", height);
-				context.scale(zoom, zoom);
-				context.drawImage(image, 0, 0);
-			
-			}
-			image.src = ev.target.result;
-		}
-		filereader.readAsDataURL(file);
-	}
-	*/
-	points = {};
 	loadAnImage();
 }
 
@@ -54,13 +26,14 @@ const loadAnImage = () => {
 			var width = 500;
 			var height = zoom * image.naturalHeight;
 			canvas.setAttribute("width", width + "px");
-			canvas.setAttribute("height", height);
+			canvas.setAttribute("height", height + "px");
 			context.scale(zoom, zoom);
 			context.drawImage(image, 0, 0);	
 		}
 		image.src = ev.target.result;
 	}
 	filereader.readAsDataURL(file);
+	points = {};
 }
 
 
@@ -100,16 +73,22 @@ canvas.addEventListener("click", (ev) => {
 			}
 		}
 	} else {
-		alert("指定できるのは四点までです");
+		var dialogResult = window.confirm("既に4点指定しています。「キャンセル」を押すとこのダイアログは閉じます。「OK」を押すと画面は初期化されます。");
+		if (dialogResult == true) {
+			initializeCanvas();
+		}
 	}
 })
 
 
-// Escキーが押されたら四角形領域を初期化する
+// キャンバスを初期化する函数
+const initializeCanvas = () => {
+	context.clearRect(0, 0, canvas.width / zoom, canvas.height / zoom);
+	loadAnImage();
+}
+
 document.addEventListener("keydown", event => {
 	if (event.code == "Escape") {
-		points = {};
-		context.clearRect(0, 0, canvas.width / zoom, canvas.height / zoom);
-		loadAnImage();
+		initializeCanvas();
 	}
 })
