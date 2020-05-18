0ed55b10e424081fba99c14fcbb1c696dfbd3122
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-05-18 18:45:34 +0900

    四角形を描く機能を追加

--- main.js
+++ main.js
@@ -50,11 +50,42 @@ window.onload = () => {
 	}
 }
 
-// マウスで座標を取得
+
+// マウスで四角形領域を描く
+var points = {};
 const canvas = document.getElementById("picture");
+const context = canvas.getContext("2d");
 canvas.addEventListener("click", (ev) => {
-	var rect = ev.target.getBoundingClientRect();
-	var x = ev.clientX - rect.left;
-	var y = ev.clientY - rect.top;
-	console.log(`${x},${y}`);
+	let n = Object.keys(points).length;
+	if (n < 4) {
+		var rect = ev.target.getBoundingClientRect();
+		var x = ev.clientX - rect.left;
+		var y = ev.clientY - rect.top;
+
+		context.beginPath();
+		context.arc(x, y, 5, 0, Math.PI * 2, 0);
+		context.fill();
+		context.stroke();
+		
+		points[n] = [x, y];
+		if (n > 0) {
+			context.beginPath();
+			context.moveTo(points[n-1][0], points[n-1][1]);
+			context.lineTo(points[n][0], points[n][1]);
+			context.closePath();
+			context.stroke();
+			if (n == 3) {
+				context.beginPath();
+				context.moveTo(points[3][0], points[3][1]);
+				context.lineTo(points[0][0], points[0][1]);
+				context.closePath();
+				context.stroke();
+
+				const output = document.getElementById("output");
+				output.value = JSON.stringify(Object.values(points), null, "\t");
+			}
+		}
+	} else {
+		alert("指定できるのは四点までです");
+	}
 })
