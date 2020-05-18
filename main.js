0784c23c24c4d0a9d04999ca67cfa8d4deb070a3
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-05-18 14:20:41 +0900

    ローカルファイルの読み込みを可能に

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
 /* main.js */
 
+// 読み込みボタンを押すと画像が読み込まれる
+const loadLocalImage = (event) => {
+	var target = event.target;
+
+	if (!target.files.length) {
+		alert("ファイルが選択されていません");
+		return;
+	}
+
+	var file = target.files[0];
+
+	const canvas = document.getElementById("picture");
+	if (canvas.getContext) {
+		const context = canvas.getContext("2d");
+		const image = new Image();
+		const filereader = new FileReader();
+
+		filereader.onload = (ev) => {
+			image.onload = () => {
+				var width = image.naturalWidth;
+				var height = image.naturalHeight;
+				canvas.setAttribute("width", width);
+				canvas.setAttribute("height", height);
+				context.drawImage(image, 0, 0);
+			
+			}
+			image.src = ev.target.result;
+		}
+		filereader.readAsDataURL(file);
+	}
+}
+
+document.getElementById("readImage").addEventListener("change", loadLocalImage, false);
+
 // canvasに画像をロードする
 window.onload = () => {
 	const canvas = document.getElementById("picture");
