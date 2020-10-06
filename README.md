37ac081d8a3ea6363d00b253bb250845ff9b836e
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-10-06 23:12:17 +0900

    Opencv.jsを用いホモグラフィ変換ができるようにした

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# 画像歪み変換ツール
+# skewedImageIron 画像歪み変換ツール
 歪んだ画像に対しホモグラフィ変換を行ない、画像をまっすぐに直すWebツールです
 
-## 構想
-- HTML+Javascriptでインターフェースをつくる
-- マウスで指定した座標とBase64変換した画像データをJSON形式にしてサーバーに送る
-- サーバーではPythonスクリプトを動かし、ホモグラフィ変換を行なう
-- 返ってきた画像をデコードし表示する
+## Usage
+```bash
+// clone this repository
+git clone https://github.com/YUUKIToriyama/skewedImageIron.git
+// do npm settings
+npm install
+// type these commands on bash, and go to localhost:8080 on your browser
+npm run test
+```
+
+## 技術的背景
+- OpenCVをブラウザ向けにコンパイルしたOpenCV.jsを活用したもの
+	- 若干重いが工夫により軽量化は可能？
--- main.js
+++ main.js
@@ -2,14 +2,13 @@
 
 var canvas;
 var scale = 1;
-var src;
-var working;
+var src, working;
 let fillColor, strokeColor;
 
 window.onload = () => {
 	document.getElementById("readImage").addEventListener("change", loadLocalImage, false);
-	fillColor = new cv.Scalar(255,255,0);
-	strokeColor = new cv.Scalar(255,255,10);
+	fillColor = new cv.Scalar(255,255,0,0.5);
+	strokeColor = new cv.Scalar(255,255,10,1);
 }
 
 // 読み込みボタンを押すと画像が読み込まれる
@@ -38,7 +37,7 @@ const loadImage = (input) => {
 		img.src = src_data;
 		img.onload = () => {
 			// opencv.jsの機能を使ってcanvasに画像を描画
-			src = cv.imread(img);
+			src = cv.imread(img, cv.IMREAD_COLOR);
 			working = src.clone();
 			cv.imshow(canvas, src);
 			//src.delete();
@@ -60,7 +59,7 @@ const drawSquare = (event) => {
 		let x = (event.clientX - rect.left) / scale;
 		let y = (event.clientY - rect.top) / scale;
 		let p = new cv.Point(parseInt(x),parseInt(y));
-		console.log(x,y);
+		//console.log(x,y);
 
 		// クリックした位置にマーカーを描画
 		cv.circle(working, p, 10, fillColor, cv.FILLED);
@@ -71,7 +70,6 @@ const drawSquare = (event) => {
 		if (0 < n) {
 			cv.line(working, points[n - 1], points[n], strokeColor, 2, cv.LINE_AA, 0);
 		}
-
 		// 4点指定したら最初の点と最後の点を結び、四角形にする
 		if (n == 3) {
 			cv.line(working, points[3], points[0], strokeColor, 2, cv.LINE_AA, 0);
@@ -79,8 +77,51 @@ const drawSquare = (event) => {
 
 		// 描画
 		cv.imshow(canvas, working);
+
+		// 画像処理(ホモグラフィ変換)
+		if (points.length == 4) {
+			let dialog = window.confirm("この四点で変換を行ないますか？");
+			if (dialog === true) {
+				rectifyImage(src, points);
+			} else {
+				// キャンセルを押したらポイントはリセット
+				cv.imshow(canvas, src);
+				points = [];
+			}
+		}
+	} else {
+		let dialog = window.confirm("既に4点指定しています。「キャンセル」を押すとこのダイアログは閉じます。「OK」を押すと画面は初期化されます。");
+		if (dialog == true) {
+			clearImage();
+			points = [];
+		}
 	}
 } 
+
+// ホモグラフィ変換で画像の歪みを補正する
+const rectifyImage = (src, points) => {
+	let w = src.rows;
+	let h = src.cols;
+	let srcArr = [];
+	points.forEach(p => {
+		srcArr.push(p.x);
+		srcArr.push(p.y);
+	});
+	let dstArr = [
+		0,0,
+		w,0,
+		w,h,
+		0,h
+	];
+	let srcTri = cv.matFromArray(4, 1, cv.CV_32FC2, srcArr);
+	let dstTri = cv.matFromArray(4, 1, cv.CV_32FC2, dstArr);
+	let M = cv.getPerspectiveTransform(srcTri, dstTri);
+	let dst = new cv.Mat();
+	let dsize = new cv.Size(w, h);
+	cv.warpPerspective(src, dst, M, dsize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, new cv.Scalar());
+	cv.imshow(canvas, dst);
+}
+
 
 // 画像を削除する
 const clearImage = () => {
@@ -94,8 +135,12 @@ const clearImage = () => {
 
 // Escを押すと画像を削除
 document.addEventListener("keydown", event => {
-	if (event.code == "Escape") {
+	if (event.code === "KeyQ") {
 		clearImage();
+		points = [];
+	} else if (event.code === "Escape") {
+		cv.imshow(canvas, src);
+		working = src.clone();
 		points = [];
 	}
 })
