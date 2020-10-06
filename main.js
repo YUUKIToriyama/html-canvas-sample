28190f562773257ec3fe81edf3e099b5340aac3c
YUUKIToriyama <ytoriyama@torisfactory.com>
2020-10-06 21:12:28 +0900

    マウスによる四点指定ができるようになった(opencv.js使用により)

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 /* main.js */
 
 var canvas;
+var scale = 1;
+var src;
+var working;
+let fillColor, strokeColor;
 
 window.onload = () => {
 	document.getElementById("readImage").addEventListener("change", loadLocalImage, false);
+	fillColor = new cv.Scalar(255,255,0);
+	strokeColor = new cv.Scalar(255,255,10);
 }
 
 // 読み込みボタンを押すと画像が読み込まれる
@@ -31,12 +37,50 @@ const loadImage = (input) => {
 		let img = new Image();
 		img.src = src_data;
 		img.onload = () => {
-			let src = cv.imread(img);
+			// opencv.jsの機能を使ってcanvasに画像を描画
+			src = cv.imread(img);
+			working = src.clone();
 			cv.imshow(canvas, src);
-			src.delete();
+			//src.delete();
+			// 画像の実際のサイズと表示のサイズの比を取っておく
+			scale = canvas.clientWidth / img.naturalWidth;
+			// canvasにイベントリスナーを設定
+			canvas.addEventListener("click", (ev) => drawSquare(ev));
 		}
 	}
 }
+
+// マウスでポイントを取る
+// 配列points内に頂点の情報を追加していく
+var points = [];
+const drawSquare = (event) => {
+	let n = points.length;
+	if (n < 4) {
+		let rect = event.target.getBoundingClientRect();
+		let x = (event.clientX - rect.left) / scale;
+		let y = (event.clientY - rect.top) / scale;
+		let p = new cv.Point(parseInt(x),parseInt(y));
+		console.log(x,y);
+
+		// クリックした位置にマーカーを描画
+		cv.circle(working, p, 10, fillColor, cv.FILLED);
+		// 配列pointsに点の情報を格納
+		points.push(p);
+		
+		// 2点目からはその点とその前の点とを結ぶ線分を描く
+		if (0 < n) {
+			cv.line(working, points[n - 1], points[n], strokeColor, 2, cv.LINE_AA, 0);
+		}
+
+		// 4点指定したら最初の点と最後の点を結び、四角形にする
+		if (n == 3) {
+			cv.line(working, points[3], points[0], strokeColor, 2, cv.LINE_AA, 0);
+		}
+
+		// 描画
+		cv.imshow(canvas, working);
+	}
+} 
 
 // 画像を削除する
 const clearImage = () => {
@@ -55,3 +99,11 @@ document.addEventListener("keydown", event => {
 		points = [];
 	}
 })
+
+// 画像処理(サンプル)
+const convertColor = () => {
+	let src = cv.imread(canvas);
+	let dst = new cv.Mat();
+	cv.cvtColor(src, dst, cv.COLOR_RGB2GRAY);
+	cv.imshow(canvas, dst);
+}
